bc4fff50a5e8afa647c46413b19e3fd7a14abcf7
jimmybombimmy <jamespeters92@gmail.com>
2023-04-06 21:52:01 +0100

    dateAfter reflecting date when increased
    The Date After on the right hand side now reflects any changes make in the boxes.
    
    To Do: Make it the same if the date decreases significantly

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //To do:
-//* Make it so that carry over
+//* Make it so that the days/months carry over to months/years if carryOver === true
+//You will probably need to make a function that 
+//* Tidy up the code a bit so that the repeating code never repeats
+//* Make it so that if you take away days and months 
+//it will work the same as if you increase them
+
 
 //------------------Today's Date
 
@@ -17,26 +22,126 @@ let beforeDate = dateBefore.valueAsDate.toDateInputValue();
 
 document.getElementById('dateBefore').addEventListener('change', function() {
     beforeDate = dateBefore.valueAsDate.toDateInputValue();
-    year = beforeDate[0] + beforeDate[1] + beforeDate[2] + beforeDate[3];
-    month = beforeDate[5] + beforeDate[6];
-    day = beforeDate[8] + beforeDate[9];
-    console.log('hi');
+    oldYear = beforeDate[0] + beforeDate[1] + beforeDate[2] + beforeDate[3];
+    oldMonth = beforeDate[5] + beforeDate[6];
+    oldDay = beforeDate[8] + beforeDate[9];
+    // console.log('hello');
 })
 
 let copyDate = new Date().toDateInputValue();
 
 
-let year = copyDate[0] + copyDate[1] + copyDate[2] + copyDate[3];
-let month = copyDate[5] + copyDate[6];
-let day = copyDate[8] + copyDate[9];
+let oldYear = copyDate[0] + copyDate[1] + copyDate[2] + copyDate[3];
+let oldMonth = copyDate[5] + copyDate[6];
+let oldDay = copyDate[8] + copyDate[9];
 
-let newYear = year;
-let newMonth = month;
-let newDay = day;
+let newYear = oldYear;
+let newMonth = oldMonth;
+let newDay = oldDay;
+let newDate = copyDate;
+
+let overYear;
+let overMonth;
+let overDay;
+
+let noYear;
+let noMonth;
+let noDay;
 
 
 let dateControl = document.querySelector('input[id="dateBefore"]');
 
+
+
+// Date 
+
+// let jan = '01'; // 31;
+// let feb = '02'; // 28;
+// let febL = '02'; // 29;
+// let mar = '03'; // 31;
+// let apr = '04'; // 30;
+// let may = '05'; // 31;
+// let jun = '06'; // 30;
+// let jul = '07'; // 31;
+// let aug = '08'; // 31;
+// let sep = '09'; // 30;
+// let oct = '10'; // 31;
+// let nov = '11'; // 30;
+// let dec = '12'; // 31;
+
+let leap;
+
+let feb;
+let febN = [0, ...Array(28).keys()].map(i => i + 1)
+let febL = [0, ...Array(29).keys()].map(i => i + 1)
+
+const monthList = [
+    [0],
+    [0, ...Array(31).keys()].map(i => i + 1),
+    // [0, ...Array(28).keys()].map(i => i + 1)],
+    [feb],
+    [0, ...Array(31).keys()].map(i => i + 1),
+    [0, ...Array(30).keys()].map(i => i + 1),
+    [0, ...Array(31).keys()].map(i => i + 1),
+    [0, ...Array(30).keys()].map(i => i + 1),
+    [0, ...Array(31).keys()].map(i => i + 1),
+    [0, ...Array(31).keys()].map(i => i + 1),
+    [0, ...Array(30).keys()].map(i => i + 1),
+    [0, ...Array(31).keys()].map(i => i + 1),
+    [0, ...Array(30).keys()].map(i => i + 1),
+    [0, ...Array(31).keys()].map(i => i + 1)
+];
+
+function dateCorrect() {
+    if (parseInt(newYear) < 10) {
+        newYear = '000' + newYear;
+    } else if (parseInt(newYear) < 100) {
+        newYear = '00' + newYear;
+    } else if (parseInt(newYear) < 1000) {
+        newYear = '0' + newYear;
+    }
+
+    if (parseInt(newMonth) < 10) {
+        newMonth = '0' + JSON.parse(parseInt(newMonth));
+    } 
+
+    if (parseInt(newDay) < 10) {
+        newDay = '0' + JSON.parse(parseInt(newDay));
+    }
+    
+}
+
+function getDate() {
+    // noMonth = parseInt(newMonth)
+    if (parseInt(newYear) % 400 === 0) {
+        feb = febL;
+    } else if (parseInt(newYear) % 100 === 0) {
+        feb = febN;
+    } else if (parseInt(newYear) % 4 === 0) {
+        feb = febL;
+    } else {
+        feb = febN;
+    }
+    if (parseInt(newMonth) > 12) {
+        // overYear = Math.floor(parseInt(newMonth) / 12);
+        newYear = parseInt(newYear) + 1;
+        newMonth = parseInt(newMonth) -12;
+        // console.log(newYear);
+        // console.log(newMonth);
+        dateCorrect();
+        getDate();
+    };
+    if (parseInt(newDay) >= monthList[parseInt(newMonth)].length) {
+        newDay = JSON.parse(parseInt(newDay) - (monthList[parseInt(newMonth)].length - 1))
+        newMonth = JSON.parse(parseInt(newMonth) + 1);
+        dateCorrect();
+        getDate();
+    }
+
+    // dateCorrect();
+    // console.log('hello')
+    // return year, month, day;
+}
 
 
 //----------------------------------------------------------------------------------------------------\\
@@ -65,14 +170,18 @@ for (let i = 0; i < numBox.length; i++) {
 // console.log(currentNumber[reali]);
 // console.log(reali);
 
+
 document.addEventListener('mouseup', function(e) {
     isDragging = false;
+    // getDate();
+    dateAfter.value = newDate
+    // console.log('hello');
     // document.removeEventListener('mouseMove', handleMouseMove)
 });
 
 
 document.addEventListener('mousemove', handleMouseMove);
-document.addEventListener('input', handleMouseMove);
+numBox[i].addEventListener('input', handleMouseMove);
 
 function handleMouseMove(e) {
     currentNumber[i] = numBox[i].innerHTML;
@@ -93,20 +202,18 @@ function handleMouseMove(e) {
         curNum = currentNumber[i]
     }
 
-    newYear = JSON.parse(parseInt(year) + parseInt(currentNumber[2]));
-    newMonth = JSON.parse(parseInt(month) + parseInt(currentNumber[1]));
-    if (parseInt(newMonth) < 10) {
-        newMonth = '0' + newMonth;
-    }
-    newDay = JSON.parse(parseInt(day) + parseInt(currentNumber[0]));
-    if (parseInt(newDay) < 10) {
-        newDay = '0' + newDay;
-    }
+    newDay = JSON.parse(parseInt(oldDay) + parseInt(currentNumber[0]));
+    newMonth = JSON.parse(parseInt(oldMonth) + parseInt(currentNumber[1]));
+    newYear = JSON.parse(parseInt(oldYear) + parseInt(currentNumber[2]));
+
+    dateCorrect();
+    getDate();
+
     newDate = newYear + '-' + newMonth + '-' + newDay;
 
-    dateAfter.toDateInputValue = newDate
+    dateAfter.value = newDate
 
-    // dateControl.value = (parseInt(year) + currentNumber[2])  + '-' + (month) + '-' + (day);
+    // dateControl.value = (parseInt(oldYear) + currentNumber[2])  + '-' + (oldmonth) + '-' + (day);
 };
 }
 //The specified value "2-2-2023" does not conform to the required format, "yyyy-MM-dd".
@@ -136,10 +243,13 @@ for (var i = 0; i < numBox.length; i++) {
     let dateClass = document.querySelectorAll('.date')
     for (j = 0; j < dateClass.length; j++) {
         dateDiv[j].style.height = classHeight[i].style.height
-        dateClass[j].style.marginTop = '44%';
+        dateClass[j].style.marginTop = '34%';
     }
     
 }
+
+// let sum = document.querySelectorAll(.plus)
+
 
 
 // datey.style.marginTop('100px');
